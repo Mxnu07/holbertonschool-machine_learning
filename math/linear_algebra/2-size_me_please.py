2c0c3c1b202fdd26f0b8473f2a50f5d12867e9ce
MMD <manuelmoralesdiaz0@gmail.com>
2024-07-30 22:11:45 -0400

    task#2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """Calculate the shape of a matrix."""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if matrix else []
+    return shape
