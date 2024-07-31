720d471e80c39a3bbc3e4aa84a22ac35322c0168
MMD <manuelmoralesdiaz0@gmail.com>
2024-07-31 19:14:07 -0400

    task #5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Function that adds two matrices element-wise."""
+
+
+def add_matrices2D(mat1, mat2):
+    """Add two matrices element-wise."""
+    # Check if both matrices have the same shape
+    if len(mat1) != len(mat2):
+        return None
+    # Add corresponding elements of both matrices
+    return [[a + b for a, b in zip(row1, row2)]
+            for row1, row2 in zip(mat1, mat2)]
