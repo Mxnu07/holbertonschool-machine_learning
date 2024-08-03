db17860318c6e020e7b620c5d6c09f34b1ac97ea
MMD <manuelmoralesdiaz0@gmail.com>
2024-08-02 20:09:19 -0400

    adding import

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Function that performs matrix multiplication."""
-
+import numpy as np
 
 def np_matmul(mat1, mat2):
     """Perform matrix multiplication."""
-    return mat1 @ mat2
+    return np.matmul(mat1, mat2)
