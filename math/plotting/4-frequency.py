3c1cd7c4e5c819437db742ce7bf8d9340d9cf732
MMD <manuelmoralesdiaz0@gmail.com>
2024-08-08 21:50:35 -0400

    histogram

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Function that plots a histogram from a dataset."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plot a histogram from a dataset."""
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Set Plot
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
+
+    # Set labels name
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+
+    # Set axis limit
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+
+    # Show plot
+    plt.show()
