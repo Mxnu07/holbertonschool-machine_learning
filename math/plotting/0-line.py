565600a55a1b5f17caffafe521426e96008fb44a
MMD <manuelmoralesdiaz0@gmail.com>
2024-08-08 21:04:59 -0400

    editing code

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -6,6 +6,12 @@ def line():
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0, 11)
 
-    plt.plot(x, y, 'r')
+    # Plot y as a solid red line
+    plt.plot(y, 'r-')
+
+    # Set x-axis limit from 0 to 10
+    plt.xlim(0, 10)
+
+    # Display the plot
+    plt.show()
