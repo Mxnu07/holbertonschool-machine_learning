af18911141726362f1228016300009c547a29124
MMD <manuelmoralesdiaz0@gmail.com>
2024-08-08 21:08:01 -0400

    documenting code

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Function that plots a line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Plot y as a solid red line."""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
