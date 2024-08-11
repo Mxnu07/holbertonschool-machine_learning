d2ec59185909360c86119c45468c8a49664b2a6c
MMD <manuelmoralesdiaz0@gmail.com>
2024-08-11 18:46:55 -0400

    task #6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
+"""Function that creates a bar graph with the quantity of fruit per person."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def bars():
+    """Create a bar graph with the quantity of fruit per person."""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     # Set Plot
-    rows = ('apples', 'bananas', 'oranges', 'peaches')
-    columns = ('Farrah', 'Fred', 'Felicia')
+    rows = ("apples", "bananas", "oranges", "peaches")
+    columns = ("Farrah", "Fred", "Felicia")
     index = columns
-    colors = ('red', 'yellow', '#ff8000', '#ffe5b4')
+    colors = ("red", "yellow", "#ff8000", "#ffe5b4")
     n_rows = len(fruit)
     bar_width = 0.5
     y_offset = np.zeros(len(columns))
 
     # Plot
     for row in range(n_rows):
-        plt.bar(index, fruit[row], bar_width, bottom=y_offset,
-                color=colors[row], label=rows[row])
+        plt.bar(
+            index,
+            fruit[row],
+            bar_width,
+            bottom=y_offset,
+            color=colors[row],
+            label=rows[row],
+        )
         y_offset = y_offset + fruit[row]
 
     # Set labels name
     plt.legend()
     plt.yticks(np.arange(0, 90, 10))
-    plt.ylabel('Quantity of Fruit')
+    plt.ylabel("Quantity of Fruit")
     plt.title("Number of Fruit per Person")
     plt.show()
