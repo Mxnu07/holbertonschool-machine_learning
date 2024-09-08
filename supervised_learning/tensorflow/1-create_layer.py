be588a11136406eea6c1ef9b9d0aa7b6dd7e6c63
MMD <manuelmoralesdiaz0@gmail.com>
2024-09-07 20:27:22 -0400

    readme & 1st tasks

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Script to create a layer"""
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    method to create a TF layer
+    Args:
+        prev: tensor of the previous layer
+        n: n nodes created
+        activation: activation function
+
+    Returns: Layer created with shape n
+
+    """
+    # Average number of inputs and output connections.
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode='FAN_AVG')
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=initializer,
+                            name='layer')
+    return layer(prev)
