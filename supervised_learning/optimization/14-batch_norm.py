df4890cd5fbb1200efe94652cc2a3ea07880bd0b
MMD <manuelmoralesdiaz0@gmail.com>
2024-09-15 13:28:44 -0400

    changing permissions

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -12,7 +12,7 @@ def create_batch_norm_layer(prev, n, activation):
     Parameters:
     - prev: activated output of the previous layer.
     - n: number of nodes in the layer to be created.
-    - activation: activation function to be applied after batch normalization.
+    - activation: activation function to be applied after batch normalization
 
     Returns:
     - A tensor of the activated output for the layer.
@@ -25,11 +25,11 @@ def create_batch_norm_layer(prev, n, activation):
 
     # Create Batch Normalization layer
     batch_norm = tf.keras.layers.BatchNormalization(
-        axis=-1,   # Apply normalization along the last axis (the feature axis)
-        momentum=0.99, 
-        epsilon=1e-7, 
-        beta_initializer=tf.keras.initializers.Zeros(),  # Initialize beta to 0
-        gamma_initializer=tf.keras.initializers.Ones()  # Initialize gamma to 1
+        axis=-1,   # Apply normalization along the last axis(the feature axis)
+        momentum=0.99,
+        epsilon=1e-7,
+        beta_initializer=tf.keras.initializers.Zeros(), # Initialize beta to 0
+        gamma_initializer=tf.keras.initializers.Ones() # Initialize gamma to 1
     )(dense)
 
     # Apply the activation function
