19185ea41331d7cde048bc5f976d3c833045e75d
MMD <manuelmoralesdiaz0@gmail.com>
2024-09-21 17:23:34 -0400

    task 0 - 1

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-"""Script to create a confusion matrix"""
+'''confusion matrix '''
+
+
 import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
-    # Convert one-hot encoded labels to class indices
-    true_labels = np.argmax(labels, axis=1)  # shape (m,)
-    pred_labels = np.argmax(logits, axis=1)  # shape (m,)
+    '''creates a confusion matrix:
 
-    # Number of classes
-    num_classes = labels.shape[1]
-
-    # Initialize the confusion matrix with zeros
-    confusion_matrix = np.zeros((num_classes, num_classes), dtype=int)
-
-    # Populate the confusion matrix
-    for i in range(len(true_labels)):
-        confusion_matrix[true_labels[i], pred_labels[i]] += 1
-
-    return confusion_matrix
+        @labels: is a one-hot numpy.ndarray of shape (m, classes) containing
+                 the correct labels for each data point
+            m is the number of data points
+            classes is the number of classes
+        @logits: is a one-hot numpy.ndarray of shape (m, classes)
+                containing the predicted labels
+        Returns: a confusion numpy.ndarray of shape (classes, classes) with
+                 row indices representing the correct labels and
+                 column indices representing the predicted labels
+    '''
+    matrix = np.matmul(labels.T, logits)
+    return matrix
