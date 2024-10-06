c065ab59e3a23a4c252708eb277d4e7f98ddcbea
MMD <manuelmoralesdiaz0@gmail.com>
2024-10-06 17:35:30 -0400

    task 4

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -6,6 +6,10 @@ using TensorFlow
 
 import tensorflow.compat.v1 as tf
 
+
+tf.disable_eager_execution()  # Disable eager execution for TensorFlow v1
+
+
 def lenet5(x, y):
     """
     Builds a modified version of LeNet-5 architecture using TensorFlow
@@ -24,54 +28,37 @@ def lenet5(x, y):
         a tensor for the loss of the network
         a tensor for the accuracy of the network
     """
-    # Disable eager execution
-    tf.disable_eager_execution()
-
-    # Initialize weights with he_normal method
-    initializer = tf.keras.initializers.VarianceScaling(scale=2.0)
 
     # C1: Convolutional layer (6 kernels, 5x5, same padding)
-    conv1 = tf.nn.conv2d(x, tf.Variable(initializer([5, 5, 1, 6])), strides=[1, 1, 1, 1], padding='SAME')
-    conv1 = tf.nn.bias_add(conv1, tf.Variable(tf.zeros([6])))
-    conv1 = tf.nn.relu(conv1)
+    conv1 = tf.layers.conv2d(x, filters=6, kernel_size=5, padding='same', activation=tf.nn.relu)
 
     # P2: Max pooling layer (2x2 kernels, 2x2 strides)
-    pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    pool1 = tf.layers.max_pooling2d(conv1, pool_size=2, strides=2)
 
     # C3: Convolutional layer (16 kernels, 5x5, valid padding)
-    conv2 = tf.nn.conv2d(pool1, tf.Variable(initializer([5, 5, 6, 16])), strides=[1, 1, 1, 1], padding='VALID')
-    conv2 = tf.nn.bias_add(conv2, tf.Variable(tf.zeros([16])))
-    conv2 = tf.nn.relu(conv2)
+    conv2 = tf.layers.conv2d(pool1, filters=16, kernel_size=5, padding='valid', activation=tf.nn.relu)
 
     # P4: Max pooling layer (2x2 kernels, 2x2 strides)
-    pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    pool2 = tf.layers.max_pooling2d(conv2, pool_size=2, strides=2)
 
     # Flatten the output
-    flatten = tf.reshape(pool2, [-1, 4*4*16])
+    flatten = tf.layers.flatten(pool2)
 
     # F5: Fully connected layer (120 nodes)
-    fc1 = tf.matmul(flatten, tf.Variable(initializer([4*4*16, 120])))
-    fc1 = tf.nn.bias_add(fc1, tf.Variable(tf.zeros([120])))
-    fc1 = tf.nn.relu(fc1)
+    fc1 = tf.layers.dense(flatten, units=120, activation=tf.nn.relu)
 
     # F6: Fully connected layer (84 nodes)
-    fc2 = tf.matmul(fc1, tf.Variable(initializer([120, 84])))
-    fc2 = tf.nn.bias_add(fc2, tf.Variable(tf.zeros([84])))
-    fc2 = tf.nn.relu(fc2)
+    fc2 = tf.layers.dense(fc1, units=84, activation=tf.nn.relu)
 
     # F7: Fully connected softmax output layer (10 nodes)
-    out = tf.matmul(fc2, tf.Variable(initializer([84, 10])))
-    out = tf.nn.bias_add(out, tf.Variable(tf.zeros([10])))
-
-    # Softmax activation
-    softmax = tf.nn.softmax(out)
+    logits = tf.layers.dense(fc2, units=10)
 
     # Loss and optimizer
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=out, labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y))
+    optimizer = tf.compat.v1.train.AdamOptimizer().minimize(loss)
 
     # Accuracy
-    correct_prediction = tf.equal(tf.argmax(out, 1), tf.argmax(y, 1))
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    return softmax, optimizer, loss, accuracy
+    return logits, optimizer, loss, accuracy
