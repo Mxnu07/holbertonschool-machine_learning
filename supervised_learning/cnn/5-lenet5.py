3e654d02d24e56d45d8a5a89073a6b1a1037fb67
MMD <manuelmoralesdiaz0@gmail.com>
2024-10-06 18:20:38 -0400

    task 5

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -9,85 +9,47 @@ from tensorflow import keras as K
 
 def lenet5(X):
     """
-    Builds a modified version of LeNet-5 architecture using Keras
-
-    parameters:
-        X [K.Input of shape (m, 28, 28, 1)]:
-            contains the input images for the network
-
-    model layers:
-    C1: convolutional layer with 6 kernels of shape (5, 5) with same padding
-    P2: max pooling layer with kernels of shape (2, 2) with (2, 2) strides
-    C3: convolutional layer with 16 kernels of shape (5, 5) with valid padding
-    P4: max pooling layer with kernels of shape (2, 2) with (2, 2) strides
-    F5: fully connected layer with 120 nodes
-    F6: fully connected layer with 84 nodes
-    F7: fully connected softmax output layer with 10 nodes
-
-    All layers requiring init should initialize kernels with he_normal method
-    All hidden layers requiring activation should use relu activation function
-
-    returns:
-        K.Model compiled to use Adam optimization (default hyperparameters)
-            and accuracy metrics
+    Builds a modified version of the LeNet-5 architecture using keras:
+    X is a K.Input of shape (m, 28, 28, 1) containing the input images for the
+        network:
+        m is the number of images
+    The model consist of the following layers in order:
+        Convolutional layer with 6 kernels of shape 5x5 with same padding
+        Max pooling layer with kernels of shape 2x2 with 2x2 strides
+        Convolutional layer with 16 kernels of shape 5x5 with valid padding
+        Max pooling layer with kernels of shape 2x2 with 2x2 strides
+        Fully connected layer with 120 nodes
+        Fully connected layer with 84 nodes
+        Fully connected softmax output layer with 10 nodes
+    All layers requiring initialization, initialize their kernels with the
+        he_normal initialization method
+    All hidden layers requiring activation should use the relu activation
+        function:
+    Returns: a K.Model compiled to use Adam optimization (with default
+        hyperparameters) and accuracy metrics
     """
 
-    # C1: Convolutional layer (6 filters of 5x5, same padding)
-    C1 = K.layers.Conv2D(
-        filters=6,
-        kernel_size=(5, 5),
-        padding="same",
-        activation="relu",
-        kernel_initializer=K.initializers.he_normal(),
-    )(X)
+    conv_1 = K.layers.Conv2D(filters=6, kernel_size=5,
+                             padding='same', activation='relu',
+                             kernel_initializer='he_normal')(X)
+    pool_1 = K.layers.MaxPool2D(2, 2)(conv_1)
 
-    # P2: Max pooling layer (2x2, stride 2)
-    P2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(C1)
+    conv_2 = K.layers.Conv2D(filters=16, kernel_size=5,
+                             activation='relu',
+                             kernel_initializer='he_normal')(pool_1)
+    pool_2 = K.layers.MaxPool2D(2, 2)(conv_2)
 
-    # C3: Convolutional layer (16 filters of 5x5, valid padding)
-    C3 = K.layers.Conv2D(
-        filters=16,
-        kernel_size=(5, 5),
-        padding="valid",
-        activation="relu",
-        kernel_initializer=K.initializers.he_normal(),
-    )(P2)
+    flatten = K.layers.Flatten()(pool_2)
+    dense_1 = K.layers.Dense(120, input_shape=X.shape,
+                             activation='relu',
+                             kernel_initializer='he_normal')(flatten)
+    dense_2 = K.layers.Dense(84, activation='relu',
+                             kernel_initializer='he_normal')(dense_1)
+    dense_3 = K.layers.Dense(10, activation='softmax',
+                             kernel_initializer='he_normal')(dense_2)
 
-    # P4: Max pooling layer (2x2, stride 2)
-    P4 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(C3)
-
-    # Flatten the output for fully connected layers
-    flatten = K.layers.Flatten()(P4)
-
-    # F5: Fully connected layer (120 nodes)
-    F5 = K.layers.Dense(
-        120,
-        activation="relu",
-        kernel_initializer=K.initializers.he_normal(),
-    )(flatten)
-
-    # F6: Fully connected layer (84 nodes)
-    F6 = K.layers.Dense(
-        84,
-        activation="relu",
-        kernel_initializer=K.initializers.he_normal(),
-    )(F5)
-
-    # F7: Fully connected layer (10 nodes with softmax activation)
-    F7 = K.layers.Dense(
-        10,
-        activation="softmax",
-        kernel_initializer=K.initializers.he_normal(),
-    )(F6)
-
-    # Build the model
-    model = K.Model(inputs=X, outputs=F7)
-
-    # Compile the model with Adam optimizer and categorical crossentropy loss
-    model.compile(
-        optimizer=K.optimizers.Adam(),
-        loss="categorical_crossentropy",
-        metrics=["accuracy"],
-    )
+    model = K.Model(inputs=X, outputs=dense_3)
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
     return model
