187630bb472e19229b63c8c07e117fbf0242b18e
MMD <manuelmoralesdiaz0@gmail.com>
2025-02-05 20:19:30 -0700

    task 5

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -4,52 +4,77 @@ Defines a function that builds a modified version of LeNet-5 architecture
 using Keras
 """
 
+
 from tensorflow import keras as K
+from tensorflow.keras import layers
 
 
 def lenet5(X):
-    """
-    Builds a modified version of the LeNet-5 architecture using keras:
-    X is a K.Input of shape (m, 28, 28, 1) containing the input images for the
-        network:
-        m is the number of images
-    The model consist of the following layers in order:
-        Convolutional layer with 6 kernels of shape 5x5 with same padding
-        Max pooling layer with kernels of shape 2x2 with 2x2 strides
-        Convolutional layer with 16 kernels of shape 5x5 with valid padding
-        Max pooling layer with kernels of shape 2x2 with 2x2 strides
-        Fully connected layer with 120 nodes
-        Fully connected layer with 84 nodes
-        Fully connected softmax output layer with 10 nodes
-    All layers requiring initialization, initialize their kernels with the
-        he_normal initialization method
-    All hidden layers requiring activation should use the relu activation
-        function:
-    Returns: a K.Model compiled to use Adam optimization (with default
-        hyperparameters) and accuracy metrics
-    """
+    """ Function that builds a modified version of the LeNet-5 architecture"""
+    initializer = K.initializers.he_normal(seed=0)
 
-    conv_1 = K.layers.Conv2D(filters=6, kernel_size=5,
-                             padding='same', activation='relu',
-                             kernel_initializer='he_normal')(X)
-    pool_1 = K.layers.MaxPool2D(2, 2)(conv_1)
+    # 1. First Convolutional Layer
+    conv1 = layers.Conv2D(
+        filters=6,
+        kernel_size=(5, 5),
+        padding='same',
+        activation='relu',
+        kernel_initializer=initializer
+    )(X)
 
-    conv_2 = K.layers.Conv2D(filters=16, kernel_size=5,
-                             activation='relu',
-                             kernel_initializer='he_normal')(pool_1)
-    pool_2 = K.layers.MaxPool2D(2, 2)(conv_2)
+    # 2. First Max Pooling Layer
+    pool1 = layers.MaxPooling2D(
+        pool_size=(2, 2),
+        strides=2
+    )(conv1)
 
-    flatten = K.layers.Flatten()(pool_2)
-    dense_1 = K.layers.Dense(120, input_shape=X.shape,
-                             activation='relu',
-                             kernel_initializer='he_normal')(flatten)
-    dense_2 = K.layers.Dense(84, activation='relu',
-                             kernel_initializer='he_normal')(dense_1)
-    dense_3 = K.layers.Dense(10, activation='softmax',
-                             kernel_initializer='he_normal')(dense_2)
+    # 3. Second Convolutional Layer
+    conv2 = layers.Conv2D(
+        filters=16,
+        kernel_size=(5, 5),
+        padding='valid',
+        activation='relu',
+        kernel_initializer=initializer
+    )(pool1)
 
-    model = K.Model(inputs=X, outputs=dense_3)
-    model.compile(optimizer='adam', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    # 4. Second Max Pooling Layer
+    pool2 = layers.MaxPooling2D(
+        pool_size=(2, 2),
+        strides=2
+    )(conv2)
+
+    # Flatten the output for Fully Connected Layers
+    flatten = layers.Flatten()(pool2)
+
+    # 5. First Fully Connected Layer
+    fc1 = layers.Dense(
+        units=120,
+        activation='relu',
+        kernel_initializer=initializer
+    )(flatten)
+
+    # 6. Second Fully Connected Layer
+    fc2 = layers.Dense(
+        units=84,
+        activation='relu',
+        kernel_initializer=initializer
+    )(fc1)
+
+    # 7. Output Layer (Softmax)
+    output = layers.Dense(
+        units=10,
+        activation='softmax',
+        kernel_initializer=initializer
+    )(fc2)
+
+    # Create the model
+    model = K.Model(inputs=X, outputs=output)
+
+    # Compile the model
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
 
     return model
