79ea3a957684304e71f23bef2f4bac9ac71c3f9a
MMD <manuelmoralesdiaz0@gmail.com>
2025-02-05 20:23:18 -0700

    removing extra import

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -6,15 +6,14 @@ using Keras
 
 
 from tensorflow import keras as K
-from tensorflow.keras import layers
 
 
 def lenet5(X):
-    """ Function that builds a modified version of the LeNet-5 architecture"""
+    """Function that builds a modified version of the LeNet-5 architecture"""
     initializer = K.initializers.he_normal(seed=0)
 
     # 1. First Convolutional Layer
-    conv1 = layers.Conv2D(
+    conv1 = K.layers.Conv2D(
         filters=6,
         kernel_size=(5, 5),
         padding='same',
@@ -23,13 +22,13 @@ def lenet5(X):
     )(X)
 
     # 2. First Max Pooling Layer
-    pool1 = layers.MaxPooling2D(
+    pool1 = K.layers.MaxPooling2D(
         pool_size=(2, 2),
         strides=2
     )(conv1)
 
     # 3. Second Convolutional Layer
-    conv2 = layers.Conv2D(
+    conv2 = K.layers.Conv2D(
         filters=16,
         kernel_size=(5, 5),
         padding='valid',
@@ -38,30 +37,30 @@ def lenet5(X):
     )(pool1)
 
     # 4. Second Max Pooling Layer
-    pool2 = layers.MaxPooling2D(
+    pool2 = K.layers.MaxPooling2D(
         pool_size=(2, 2),
         strides=2
     )(conv2)
 
     # Flatten the output for Fully Connected Layers
-    flatten = layers.Flatten()(pool2)
+    flatten = K.layers.Flatten()(pool2)
 
     # 5. First Fully Connected Layer
-    fc1 = layers.Dense(
+    fc1 = K.layers.Dense(
         units=120,
         activation='relu',
         kernel_initializer=initializer
     )(flatten)
 
     # 6. Second Fully Connected Layer
-    fc2 = layers.Dense(
+    fc2 = K.layers.Dense(
         units=84,
         activation='relu',
         kernel_initializer=initializer
     )(fc1)
 
     # 7. Output Layer (Softmax)
-    output = layers.Dense(
+    output = K.layers.Dense(
         units=10,
         activation='softmax',
         kernel_initializer=initializer
