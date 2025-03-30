343e316e6c6b2f27b43991379e01acd2c40e6e3e
MMD <manuelmoralesdiaz0@gmail.com>
2025-03-29 20:49:58 -0400

    renaming task

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -74,3 +74,39 @@ def intersection(x, n, P, Pr):
     intersection = likeli * Pr
 
     return intersection
+
+
+def marginal(x, n, P, Pr):
+    """This method calculates the marginal probability of obtaining the data
+    Args:
+        x is the number of patients that develop severe side effects
+        n is the total number of patients observed
+        P is a 1D numpy.ndarray containing the various hypothetical
+        probabilities of developing severe side effects
+        Pr is a 1D numpy.ndarray containing the prior beliefs of P
+        Returns: the marginal probability of obtaining x and n
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x <= 0:
+        error = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(error)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    # Intersection of obtaining x and n with each probability in P
+    inter = intersection(x, n, P, Pr)
+    # Marginal probability of obtaining x and n
+    marginal = np.sum(inter)
+
+    return marginal
